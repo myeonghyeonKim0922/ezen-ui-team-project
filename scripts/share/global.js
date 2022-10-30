4f22e7c6168632d31051d32c4a4025c150d52ee3
nsunf <jun5013@naver.com>
2022-10-30 13:23:18 +0900

    responsive web

--- scripts/share/global.js
+++ scripts/share/global.js
@@ -1,6 +1,6 @@
 // set min-height of main section
 function setMainHeight() {
-  const header = document.querySelector(".header");
+  const header = document.querySelector(".header__main");
   const footer = document.querySelector(".footer");
   const main = document.querySelector(".main");
 
@@ -31,9 +31,37 @@ function setGnbSelected() {
   }
 }
 
+// set mobile menu button
+
+function setMenuButton() {
+  const menuBtn = document.querySelector(".menu__button");
+
+  menuBtn.addEventListener("click", () => {
+    const isOpen = menuBtn.classList.toggle("menu__button--open");
+
+    toggleMenu(isOpen);
+  })
+}
+
+function toggleMenu(on) {
+  const menu = document.querySelector(".menu");
+  if (on) {
+    menu.style.display = "block";
+    setTimeout(() => {
+      menu.style.top = "60px";
+    }, 100);
+  } else {
+    menu.style.top = "-60px";
+    setTimeout(() => {
+      menu.style.display = "none";
+    }, 500);
+  }
+}
+
 (function main() {
   setMainHeight();
   setGnbSelected();
+  setMenuButton();
 
   addEventListener("resize", setMainHeight);
 })();
