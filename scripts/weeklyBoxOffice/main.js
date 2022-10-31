51d95046c5e72d1522519b3e86ad4b55ae406459
github myeonghyeonKim0922echo <cdeo7788@naver.com>
2022-10-31 15:43:43 +0900

    레이아웃 추가

---
+++ scripts/weeklyBoxOffice/main.js
@@ -0,0 +1,11 @@
+const content = document.querySelector(main__content)
+
+//영화 카드
+function movie__card(){
+content.style.width = "200px";
+content.style.height = "250px";
+
+}   
+
+
+movie__card()
