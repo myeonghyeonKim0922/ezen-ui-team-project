e19af615452c1e50502c06627c893854627f1e32
nsunf <jun5013@naver.com>
2022-11-02 15:17:19 +0900

    fetch example

--- example/scripts/page1.script.js
+++ example/scripts/page1.script.js
@@ -1,46 +1,39 @@
-const kobisKey = "6e366ce8b5c64c17cdc1520bdc6af5d8";
-
-const kobisURL = {
-  daily: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json",
-  search: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json",
-  detail: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json"
+const apiUrl = {
+  daily: "http://13.125.36.145:8080/boxoffice/daily",
+  weekly: "http://13.125.36.145:8080/boxoffice/weekly",
+  list: "http://13.125.36.145:8080/movie/list",
+  info: "http://13.125.36.145:8080/movie/info"
 }
 
-const customAPIURL = {
-  info: "http://13.125.36.145:8080/search/movie/info",
-  poster: "http://13.125.36.145:8080/search/movie/poster"
-}
-
-async function getDailyBoxOffice() {
-  const dailyUrl = `${kobisURL.daily}?key=${kobisKey}&targetDt=${"20221031"}`;
+async function getDailyBoxOffice(targetDt) {
+  const dailyUrl = apiUrl.daily + "?targetDt=" + targetDt;
   const dailyData = await fetch(dailyUrl).then(res => res.json());
-  const dailyMovieList = dailyData.boxOfficeResult.dailyBoxOfficeList;
+  const dailyMovieList = dailyData.dailyBoxOfficeList;
 
-  dailyMovieList.map(async (movie) => {
-    const detailUrl = `${kobisURL.detail}?key=${kobisKey}&movieCd=${movie.movieCd}`;
-    const detailData = await fetch(detailUrl).then(res => res.json());
-    const detail = detailData.movieInfoResult.movieInfo;
-
-    const dirs = detail.directors;
-    let dirNames = "";
-    if (dirs.length > 1) {
-      dirNames = detail.directors.reduce((lhs, rhs) => lhs.peopleNm + "|" + rhs.peopleNm);
-    } else if (dirs.length == 1) {
-      dirNames = dirs[0].peopleNm;
-    }
-
-    const posterUrl = `${customAPIURL.poster}?title=${movie.movieNm}&dirs=${dirNames}`;
-    const posterData = await fetch(posterUrl).then(res => res.json());
-    if (posterData.result === "success") {
-      const posterSrc = posterData.data.posterSrc ?? undefined;
-      createCard(movie, posterSrc);
-    } else {
-      createCard(movie);
-    }
+  dailyMovieList.forEach((movie, i) => {
+    if (i === 0)
+      setMainResult(movie);
+    createCard2(movie)
   })
 }
 
-function createCard(movie, posterSrc = "") {
+async function getMovieList(movieNm) {
+  const url = apiUrl.list + "?movieNm=" + movieNm;
+  const data = await fetch(url).then(res => res.json());
+  const movieList = data.movieList;
+
+  document.querySelector(".result").innerHTML = "";
+  movieList.forEach(movie => createCard2(movie));
+}
+
+function search() {
+  const searchTerm = document.querySelector(".search__input").value;
+  if (searchTerm.replaceAll(" ", "") !== "") {
+    getMovieList(searchTerm);
+  }
+}
+
+function createCard(movie) {
   const card = document.createElement("div");
   const cardLink = document.createElement("a");
 
@@ -65,7 +58,7 @@ function createCard(movie, posterSrc = "") {
   rank.innerText = movie.rank;
   title.innerText = movie.movieNm;
   openDt.innerText = movie.openDt;
-  posterImg.setAttribute("src", posterSrc);
+  posterImg.setAttribute("src", movie.poster);
 
   card.appendChild(cardLink);
 
@@ -79,6 +72,68 @@ function createCard(movie, posterSrc = "") {
   document.querySelector(".result").appendChild(card);
 }
 
+function createCard2(movie) {
+  const list = document.querySelector(".result");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <a class="card__link" href="/example/page2.html#${movie.movieCd}">
+      <div class="card__rank">${movie.rank}</div>
+      <div class="card__poster">
+        <img class="card__poster--img" src="${movie.poster}" alt="card-poster">
+      </div>
+      <div class="card__info info">
+        <div class="info__title">${movie.movieNm}</div>
+        <div class="info__open-dt">${movie.openDt}</div>
+      </div>
+    </a>
+  `;
+
+  list.appendChild(card);
+}
+
+function setMainResult(movie) {
+  const poster = document.querySelector(".main-result__poster--img");
+  const title = document.querySelector(".info__item--title");
+  const salesAcc = document.querySelector(".info__item--sales-acc");
+  const audiAcc = document.querySelector(".info__item--audi-acc");
+
+  const megaboxLink = document.querySelector(".link__theater--mega");
+  const cgvLink = document.querySelector(".link__theater--cgv");
+
+  poster.setAttribute("src", movie.poster);
+  title.innerText = movie.movieNm;
+  salesAcc.innerText = numToString(movie.salesAcc) + "원";
+  audiAcc.innerText = numToString(movie.audiAcc) + "명";
+
+  megaboxLink.setAttribute("href", "https://www.megabox.co.kr/movie?searchText=" + movie.movieNm);
+  cgvLink.setAttribute("href", "http://www.cgv.co.kr/search/?query=" + movie.movieNm);
+}
+
+function numToString(num) {
+  const strArr = num.toString().split("");
+
+  const arr = [];
+  for (let i = strArr.length - 1; i > 0; i--) {
+    if ((arr.length + 1) % 4 === 0) {
+      arr.unshift(",");
+    }
+    arr.unshift(strArr[i]);
+  }
+
+  return arr.join("");
+}
+
 (function main() {
-  getDailyBoxOffice();
+  getDailyBoxOffice("20221101");
+
+  document.querySelector(".search__input").addEventListener("keydown", e => {
+    if (e.key === "Enter")
+      search();
+  })
+  document.querySelector(".search__btn").addEventListener("click", e => {
+    search();
+  })
 })();
