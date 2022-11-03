6daab188aa89588b7135383b09e8fc3ec492bded
github myeonghyeonKim0922echo <cdeo7788@naver.com>
2022-11-03 18:26:02 +0900

    날짜넘김 넘길때 숫자 변경 js다소 수정

--- scripts/weeklyBoxOffice/main.js
+++ scripts/weeklyBoxOffice/main.js
@@ -1,76 +1,117 @@
-let url = "http://localhost:8080/boxoffice/weekly?targetDt=20221020";
-let url = "http://13.125.36.145:8080/boxoffice/weekly?targetDt=20221030";
+const ltbutton = document.getElementById(".lt__button");
+const gtbutton = document.getElementById(".gt__button");
+var today = new Date();
+var setdate = new Date(today);
 
-let data = 0;
-let adata = 0;
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    return data.weeklyBoxOfficeList;
-  })
-  .then((adata) => {
-    console.log(adata);
-    adata.forEach((data) => {
-      movieCard(data);
+var newdate = 0;
+var ltdate = 0;
+
+setdate.setDate(today.getDate() - 7);
+var year = "" + setdate.getFullYear();
+var month = ("0" + (1 + setdate.getMonth())).slice(-2);
+var day = ("0" + setdate.getDate()).slice(-2);
+const movieday = `${year}${month}${day}`;
+
+console.log(movieday);
+
+function lt() {
+  const datefont = document.querySelector(".date__font");
+  setdate.setDate(setdate.getDate() - 7);
+  console.log(setdate);
+
+  var year = "" + setdate.getFullYear();
+  var month = ("0" + (1 + setdate.getMonth())).slice(-2);
+  var day = ("0" + setdate.getDate()).slice(-2);
+  const movieday = `${year}${month}${day}`;
+  var ltfont = ("0" + (setdate.getDate() - 3)).slice(-2);
+  var gtfont = ("0" + (setdate.getDate() + 3)).slice(-2);
+  console.log(movieday);
+  datefont.innerText = `${year}.${month}.${ltfont} ~ ${year}.${month}.${gtfont} `;
+  apiload(movieday);
+}
+
+function gt() {
+  const datefont = document.querySelector(".date__font");
+  setdate.setDate(setdate.getDate() + 7);
+  console.log(setdate);
+
+  var year = "" + setdate.getFullYear();
+  var month = ("0" + (1 + setdate.getMonth())).slice(-2);
+  var day = ("0" + setdate.getDate()).slice(-2);
+  const movieday = `${year}${month}${day}`;
+  var ltfont = ("0" + (setdate.getDate() - 3)).slice(-2);
+  var gtfont = ("0" + (setdate.getDate() + 3)).slice(-2);
+  console.log(ltfont);
+  // console.log(movieday);
+  datefont.innerText = `${year}.${month}.${ltfont} ~ ${year}.${month}.${gtfont} `;
+  apiload(movieday);
+}
+
+// let url = "http://localhost:8080";
+// url += "/boxoffice/weekly";
+// url += "?targetDt=";
+// url += getToday();
+
+// console.log(url);
+
+function apiload(movieday) {
+  let url = "http://13.125.36.145:8080";
+  url += "/boxoffice/weekly";
+  url += "?targetDt=";
+  // url += "/dummy";
+  url += movieday;
+
+  let data = 0;
+  let adata = 0;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      return data.weeklyBoxOfficeList;
+    })
+    .then((adata) => {
+      document.querySelector(".card__list").innerHTML = "";
+      adata.forEach((data) => {
+        movieCard(data);
+      });
+      swiperPoster(adata);
     });
-    swiperPoster(adata);
-  });
+}
 
 function movieCard(datalist) {
   const cardwrap = document.createElement("li");
-  const cardFrame = document.createElement("div");
-  const navMovieInfo = document.createElement("a");
-  const moviePoster = document.createElement("div");
-  const poster2 = document.createElement("img");
-  const movieInfo = document.createElement("div");
-  const infoList = document.createElement("ul");
-  const list1 = document.createElement("li");
-  const list2 = document.createElement("li");
-  const list3 = document.createElement("li");
-  const list4 = document.createElement("li");
-  const list5 = document.createElement("li");
-  const list6 = document.createElement("li");
-
-  cardFrame.classList.add("movie__card");
-  navMovieInfo.classList.add("nav__movieinfo");
-  moviePoster.classList.add("movie__poster");
-  poster2.classList.add("poster2");
-  movieInfo.classList.add("movie__info");
-  infoList.classList.add("info__list");
-  list1.classList.add("list__1");
-  list2.classList.add("list__2");
-  list3.classList.add("list__3");
-  list4.classList.add("list__4");
-  list5.classList.add("list__5");
-  list6.classList.add("list__6");
-
-  list1.innerText = datalist.movieNm;
-  list2.innerText = datalist.openDt;
-  list3.innerText = datalist.salesAcc + "원";
-  list5.innerText = datalist.salesAmt + "명";
-  poster2.setAttribute("src", datalist.poster);
-
+  cardwrap.classList.add("card__wrap");
   const cardList = document.querySelector(".card__list");
   cardList.appendChild(cardwrap);
-  cardwrap.appendChild(cardFrame);
-  cardFrame.appendChild(navMovieInfo);
-  cardFrame.appendChild(moviePoster);
-  moviePoster.appendChild(poster2);
-  cardFrame.appendChild(movieInfo);
-  movieInfo.appendChild(infoList);
-  infoList.appendChild(list1);
-  infoList.appendChild(list2);
-  infoList.appendChild(list3);
-  infoList.appendChild(list4);
-  infoList.appendChild(list5);
-  infoList.appendChild(list6);
+
+  cardwrap.innerHTML = ` 
+    <div class="movie__card">
+      <a href="" class="nav__movieinfo">
+      <div class="movie__poster">
+        <img src="${datalist.poster}" class="poster2">
+      </div>
+      <div class="movie__info">
+        <ul class="info__list">
+          <li class="list__1">${datalist.movieNm}</li>
+          <li class="list__2">${datalist.openDt}</li>
+          <li class="list__3">${datalist.salesAcc}</li>
+          <li class="list__4"></li>
+          <li class="list__5">${datalist.salesAmt}</li>
+          <li class="list__6"></li>
+        </ul>
+      </div>
+      </a>
+    </div>
+`;
 }
 
 function swiperPoster(datalist) {
   const slides = document.querySelectorAll(".swiper-slide");
   slides.forEach((slide, i) => {
-    const img = document.createElement("img");
-    img.setAttribute("src", datalist[i].poster);
-    slide.appendChild(img);
+    slide.innerHTML = `<img src="${datalist[i].poster}" />`;
+    // const img = document.createElement("img");
+    // img.setAttribute("src", datalist[i].poster);
+    // slide.appendChild(img);
   });
 }
+
+apiload(movieday);
