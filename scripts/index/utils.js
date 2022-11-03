4c5c4e1e0ab8631b0618f9be54e0b7cd09ddcbd2
nsunf <jun5013@naver.com>
2022-11-03 11:31:36 +0900

    add canvas

---
+++ scripts/index/utils.js
@@ -0,0 +1,13 @@
+export function getDist(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+export function debounce(callback, limit = 100) {
+  let timeout
+  return function(...args) {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+          callback.apply(this, args)
+      }, limit)
+  }
+}
